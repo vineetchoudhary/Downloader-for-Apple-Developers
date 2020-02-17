d98f3826dabc8f759222bd374996cdf925eb0e45
Vineet Choudhary <vineetchoudhary@live.in>
2020-02-17 15:28:28 +0530

    Initial Commit

---
+++ DeveloperToolsDownloader/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  DeveloperToolsDownloader
+//
+//  Created by Vineet Choudhary on 17/02/20.
+//  Copyright © 2020 Developer Insider. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
