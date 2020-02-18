682617558cd9a4f93120c34d31c26d5791426c42
Vineet Choudhary <vineetchoudhary@live.in>
2020-02-18 20:27:32 +0530

    Added installer file

---
+++ install.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+VERSION="1.0.1"
+APP_NAME="Downloader.app"
+FILE_NAME="Downloader.tar.gz"
+FILE_URL="https://github.com/vineetchoudhary/Downloader-for-Apple-Developer/releases/download/$VERSION/AppBox.tar.gz"
+APPLICATION_DIR='/Applications'
+
+echo "Downloading Downloader for Apple Developers $VERSION..."
+curl -OL $FILE_URL
+echo "Installing Downloader for Apple Developers $VERSION..."
+tar -xf $FILE_NAME -C $APPLICATION_DIR
+echo "Starting Downloader for Apple Developers $VERSION..."
+open $APPLICATION_DIR/$APP_NAME
