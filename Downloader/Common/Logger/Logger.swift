67e0d095ee3bb7b733a9e7d1e2a0e9cb04c6dc00
Vineet Choudhary <vineetchoudhary@live.in>
2020-05-18 16:19:26 +0530

    Added logger to log start and finish events

---
+++ Downloader/Common/Logger/Logger.swift
@@ -0,0 +1,34 @@
+//
+//  Logger.swift
+//  Downloader
+//
+//  Created by Vineet Choudhary on 18/05/20.
+//  Copyright © 2020 Developer Insider. All rights reserved.
+//
+
+import Foundation
+import AppCenterAnalytics
+
+struct Logger {
+    static func downloadStart(url: String) {
+        let params = getParams(from: url)
+        MSAnalytics.trackEvent("Download Start", withProperties: params)
+    }
+    
+    static func downloadFinish(url: String) {
+        let params = getParams(from: url)
+        MSAnalytics.trackEvent("Download Finish", withProperties: params)
+    }
+    
+    private static func getParams(from urlString: String)-> [String: String] {
+        var params = [String: String]()
+        guard let url = URL(string: urlString) else {
+            params["full_url"] = urlString
+            return params
+        }
+        
+        params["full_url"] = url.absoluteString
+        params["file_name"] = url.lastPathComponent
+        return params;
+    }
+}
