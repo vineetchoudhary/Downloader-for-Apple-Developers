6a16c1451fae290b1fb66ef1b7b457d9f95951a0
Vineet Choudhary <vineetchoudhary@live.in>
2020-06-14 18:05:30 +0530

    Added download process complete event

---
+++ Downloader/ViewControllers/DownloaderControllers/DownloadProcessCell/DownloadProcessCompleteTableCellView.swift
@@ -0,0 +1,20 @@
+//
+//  DownloadProcessCompleteTableCellView.swift
+//  Downloader
+//
+//  Created by Vineet Choudhary on 14/06/20.
+//  Copyright © 2020 Developer Insider. All rights reserved.
+//
+
+import Cocoa
+
+class DownloadProcessCompleteTableCellView: NSTableCellView {
+
+    @IBOutlet weak var nameLabel: NSTextField!
+    @IBOutlet weak var messageLabel: NSTextField!
+    
+    func config(downloadProgress: DownloadProgress) {
+        nameLabel.stringValue = downloadProgress.fileName ?? "--"
+        messageLabel.stringValue = "Download Completed."
+    }
+}
