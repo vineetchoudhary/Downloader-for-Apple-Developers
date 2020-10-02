a1bd85db00b50730185fbb1b13d21fadb5d3b02e
Vineet Choudhary <vineetchoudhary@live.in>
2020-10-02 11:48:58 +0530

    Removed hardcoded error string and used localisations strings

--- Downloader/ViewControllers/DownloaderControllers/DownloadProcessCell/DownloadProcessCompleteTableCellView.swift
+++ Downloader/ViewControllers/DownloaderControllers/DownloadProcessCell/DownloadProcessCompleteTableCellView.swift
@@ -15,12 +15,9 @@ class DownloadProcessCompleteTableCellView: NSTableCellView {
     
     func config(downloadProgress: DownloadProgress) {
         nameLabel.stringValue = downloadProgress.fileName ?? "--"
-        messageLabel.maximumNumberOfLines = 6
-        if let output = downloadProgress.output?.output, let error = downloadProgress.output?.error {
-            let finalOutput = "\(output)\n\n\(error)"
-            messageLabel.stringValue = finalOutput
-        } else {
-            messageLabel.stringValue = NSLocalizedString("DownloadCompleted", comment: "")
+        messageLabel.maximumNumberOfLines = 0
+        if let output = downloadProgress.output?.output {
+            messageLabel.stringValue = output
         }
     }
 }
