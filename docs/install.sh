c30031c6f9afd557bf810bb2a94e6b9b5832a6b0
Vineet Choudhary <vineetchoudhary@live.in>
2023-02-20 10:09:42 +0530

    Update install.sh to download aria2c separately

--- docs/install.sh
+++ docs/install.sh
@@ -1,13 +1,25 @@
 #!/bin/sh
 
 VERSION="2.2.0"
-APP_NAME="Downloader.app"
-FILE_NAME="Downloader.tar.gz"
-FILE_URL="https://github.com/vineetchoudhary/Downloader-for-Apple-Developer/releases/download/$VERSION/$FILE_NAME"
+APPBOX_APP_NAME="Downloader.app"
+APPBOX_GITHUB_FILE_NAME="Downloader.tar.gz"
+GITHUB_RELEASES_BASE_URL="https://github.com/vineetchoudhary/Downloader-for-Apple-Developer/releases/download/$VERSION"
+APPBOX_FULL_FILE_URL="$GITHUB_RELEASES_BASE_URL/$APPBOX_GITHUB_FILE_NAME"
+
+ARIA2C_APP_NAME="aria2c"
+ARIA2C_GITHUB_FILE_NAME="aria2c"
+ARIA2C_FULL_FILE_URL="$GITHUB_RELEASES_BASE_URL/$ARIA2C_GITHUB_FILE_NAME"
+
+ARIA2C_PARH='Contents/Resources/aria2c'
 APPLICATION_DIR='/Applications'
 
+
 echo "Downloading Downloader for Apple Developers $VERSION..."
-curl -OL $FILE_URL
+curl -OL $APPBOX_FULL_FILE_URL
 echo "Installing Downloader for Apple Developers $VERSION..."
-tar -xf $FILE_NAME -C $APPLICATION_DIR
-echo "Launch $APP_NAME from your Applications directory."
+tar -xf $APPBOX_GITHUB_FILE_NAME -C $APPLICATION_DIR
+echo "Download aria2c $VERSION..."
+curl -OL $ARIA2C_FULL_FILE_URL
+echo "Installing aria2c $VERSION..."
+mv $ARIA2C_GITHUB_FILE_NAME $APPLICATION_DIR/$APPBOX_APP_NAME/$ARIA2C_PARH
+echo "Launch $APPBOX_APP_NAME from your Applications directory."
